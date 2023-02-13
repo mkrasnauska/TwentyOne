c09a484d273999ec735cf70a5d9da50b1d8b1b67
Margarita <margaritakrasnauska@gmail.com>
2023-02-13 14:13:50 +0000

    Add project files.

---
+++ TwentyOne/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Card cardOne= new Card();
+            //cardOne.Face = "Queen";
+            //cardOne.Suit = "Spades";
+            Console.WriteLine(cardOne.Face+ " Of "+cardOne.Suit);
+            Console.ReadLine();
+        }
+    }
+}
